949e04e8b75bfd1a17c62299ea4d2859985cecbb
Francisco <franciscodiakoma@gmail.com>
2024-12-20 04:56:30 +0100

    fix

--- src/services/validationAdmin.ts
+++ src/services/validationAdmin.ts
@@ -4,7 +4,7 @@ import validator from 'validator'
 export function isAvaliableAdmin(admin : IAdmin) {
     try {
         
-        if (validator.isEmail(admin.email) && admin.name?.length >= 4 && !isNaN(admin.adress.cep) && admin.adress.city?.length > 4 && admin.adress.qoute?.length >0) {
+        if (validator.isEmail(admin.email)) {
             return true
         } else {
             return false
